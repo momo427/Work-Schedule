b203d562635348bcf8c4e837ad6ddebbda0a0b8e
momogit <ferguson.tyra@gmail.com>
2021-03-18 23:08:50 -0400

    Start of script, css, and correction on link to script in html plus class corrections

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,17 @@ $('#currentDay').append(currentDay);
 
 var currentTime = moment().format("LT");
 $('#currentTime').append(currentTime);
+
+//functions for color coded timeblocks, past,present,future
+var element = document.getElementById("currentTime").value;
+var time = new Date.getHours();
+
+function colorCodedTime () {
+    if (element-time > 9) {
+        $("#nine").addClass("past");
+    } else if (time >= 9 && now < 10) {
+        $("#nine").addClass("present");
+    }else if (time < 9) {
+        $("#nine").addClass("future");
+    }
+}
